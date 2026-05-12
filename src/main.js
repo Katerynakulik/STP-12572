4af984adc9e3bcebf20bbbfba57568b06da5a3db
Kateryna Kulik <katerynakulik4@gmail.com>
2026-05-12 14:36:04 +0200

    Configure gallery for mobile and desktop

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import 'swiper/css/pagination';
 // Обробка скролу для хедера (фон)
 const header = document.querySelector('#main-header');
 window.addEventListener('scroll', () => {
-    header.classList.toggle('header--scrolled', window.scrollY > 50);
+  header.classList.toggle('header--scrolled', window.scrollY > 50);
 });
 // Swiper
 const swiper = new Swiper('.gallery-swiper', {
@@ -22,13 +22,13 @@ const swiper = new Swiper('.gallery-swiper', {
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next-custom',
+    prevEl: '.swiper-button-prev-custom',
   },
   // Вимикаємо Swiper на десктопі
   breakpoints: {
     1440: {
       enabled: false,
-    }
-  }
+    },
+  },
 });
